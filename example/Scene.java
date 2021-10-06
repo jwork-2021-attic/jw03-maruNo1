35fdc4fb9ac53cd1c1a1bc97f936968173d7f5a6
maruNo1 <57053150+maruNo1@users.noreply.github.com>
2021-10-06 10:09:35 +0800

    Add files via upload

---
+++ example/Scene.java
@@ -0,0 +1,44 @@
+package example;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.net.URL;
+
+import example.classloader.SteganographyClassLoader;
+
+public class Scene {
+
+    public static void main(String[] args) throws Exception {
+
+        Line line = new Line(7);
+        line.put(Gourd.ONE, 6);
+        line.put(Gourd.TWO, 3);
+        line.put(Gourd.THREE, 1);
+        line.put(Gourd.FOUR, 5);
+        line.put(Gourd.FIVE, 2);
+        line.put(Gourd.SIX, 4);
+        line.put(Gourd.SEVEN, 0);
+
+        Geezer theGeezer = Geezer.getTheGeezer();
+
+        SteganographyClassLoader loader = new SteganographyClassLoader(
+                new URL("File:///D:/jwork-2021/jw03-maruNo1/example.QuickSorter.png"));
+
+        // 加载从隐写术图中读取出的example.InsertSoerter类
+        Class c = loader.loadClass("example.QuickSorter");
+
+        Sorter sorter = (Sorter) c.newInstance();
+
+        theGeezer.setSorter(sorter);
+
+        String log = theGeezer.lineUp(line);
+
+        BufferedWriter writer;
+        writer = new BufferedWriter(new FileWriter("result.txt"));
+        writer.write(log);
+        writer.flush();
+        writer.close();
+
+    }
+
+}
